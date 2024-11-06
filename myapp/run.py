181c1dfe3df5d2ba4a0e1a475c247651fa6048dc
Marek <nm53902@zut.edu.pl>
2024-11-06 10:29:19 +0100

    poprawki | struktura danych

--- myapp/run.py
+++ myapp/run.py
@@ -4,7 +4,6 @@ from app import create_app
 
 # Create an instance of the Flask app
 app = create_app()
-
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1",port=8080,debug=True)
