6fd66a5cec69a521fe2aaa707e754159adbd3e36
Marek <nm53902@zut.edu.pl>
2024-11-11 17:33:33 +0100

    flask

--- myapp/app/routes.py
+++ myapp/app/routes.py
@@ -1,5 +1,3 @@
-# /app/routes.py
-
 from flask import Blueprint, render_template, request, redirect, url_for, session
 
 main_routes = Blueprint('main', __name__)
@@ -7,21 +5,23 @@ main_routes = Blueprint('main', __name__)
 @main_routes.route('/')
 def index():
     if 'username' in session:
-        return f'Logged in as {session["username"]}'
+        return render_template('index.html', username=session["username"])
     return redirect(url_for('main.login'))
 
 @main_routes.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        session['username'] = request.form['username']
+        username = request.form['username']
+        session['username'] = username
         return redirect(url_for('main.index'))
-    return "Hello World" #render_template('login.html')
+    return render_template('login.html')  # Wyświetla formularz logowania
 
 @main_routes.route('/logout')
 def logout():
     session.pop('username', None)
-    return redirect(url_for('main.index'))
+    return redirect(url_for('main.login'))
 
-@main_routes.errorhandler(404)
+# Obsługa błędu 404
+@main_routes.app_errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
