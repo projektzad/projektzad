bd4da7523760ba96a23e3c5dd465bf56a2dbbff9
Marek <nm53902@zut.edu.pl>
2025-06-09 19:29:13 +0200

    porpawka do install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Przejdź do katalogu aplikacji
-cd projektzad/myapp || exit 1
+cd myapp || exit 1
 
 # Zainstaluj zależności
 pip install -r requirements.txt
@@ -9,4 +9,4 @@ pip install -r requirements.txt
 # Uruchom aplikację Flask przez Gunicorn w tle
 nohup gunicorn -w 4 run:app --bind 0.0.0.0:5000 > gunicorn.log 2>&1 &
 
-echo "Aplikacja uruchomiona. Log: projektzad/myapp/gunicorn.log"
+echo "Aplikacja uruchomiona. Log: myapp/gunicorn.log"
